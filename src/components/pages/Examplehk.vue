48a82a0a2b79333b71e05046671ff5c506b0805c
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-08-31 20:07:00 +0300

    Renamed from Contact

---
+++ src/components/pages/Examplehk.vue
@@ -0,0 +1,20 @@
+<template>
+  <main-layout>
+    <h3>Example destroy</h3>
+  </main-layout>
+</template>
+
+<script>
+export default {
+  name: "ExampleDestroy",
+  components: {},
+};
+</script>
+<style lang="scss">
+.notfound-wrapper {
+  padding: 20px;
+  h3 {
+    color: red;
+  }
+}
+</style>

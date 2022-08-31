f7304f3607ac2b80bf05c43c93ef974375f1b044
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-08-31 23:56:49 +0300

    Added blog component

---
+++ src/components/pages/blogs/BlogThree.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h5>CSS Tricks</h5>
+    <p>
+      Despite having CSS in the title, CSS-Tricks focuses on all aspects of
+      Frontend development. You may know them from their world famous "A
+      Complete Guide to Flexbox" article, you only have to google flexbox for
+      this to be top. They show off lots of new JS features and post a bunch of
+      tutorials on how to things.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogThree",
+};
+</script>
+
+<style lang="scss">
+@import "../../../assets/styles.scss";
+</style>

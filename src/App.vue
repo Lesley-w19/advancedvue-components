3e17002abc62cd28c7b134b697a81552b50bedc8
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-08-31 18:32:23 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <button @click="goToPage('/about')">Go to About</button>
+    <button @click="goToPage('/')">Go to Home</button>
+    <app-router></app-router>
+  </div>
+</template>
+
+<script>
+import AppRouter from "./components/Router.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      imgs: this.$root.images,
+    };
+  },
+  components: {
+    AppRouter,
+  },
+  methods: {
+    goToPage(route) {
+      window.location = route;
+    },
+  },
+  provide: function(){
+    return {
+      getImages: this.imgs
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  el: "#app",
+  data: {
+    list: ["home", "about", "contact", "blogs"],
+    images: [
+      "https://images.pexels.com/photos/13189931/pexels-photo-13189931.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/12699167/pexels-photo-12699167.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/13085571/pexels-photo-13085571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    ],
+  },
+  render: (h) => h(App),
+}).$mount("#app");

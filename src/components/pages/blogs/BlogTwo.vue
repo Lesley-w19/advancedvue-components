164f1d0422d153f5261f5c1aee26f14950233da5
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-08-31 23:56:55 +0300

    Added blog component

---
+++ src/components/pages/blogs/BlogTwo.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<h5>Development JS</h5>
+<p>With two decades of improvement, JavaScript has become one of the most popular programming languages of all time. The journey started in 1995 when Brendan Eich created JavaScript in just 10 days. From there, it has seen multiple revisions, drafts, and growth in the form of frameworks, API’s, modules, etc. Today, we will go forward and list the top JavaScript blogs from the internet so that you can enjoy the lastest development in the field of JavaScript.
+
+According to RedMonk programming language rankings and GitHut.info, JavaScript is leading the pack in the terms of repositories and the most discussed programming language on StackOverFlow. The numbers itself speaks about the future of JavaScript as it has grown beyond the initial capabilities of simple DOM manipulations.</p>
+</div>
+</template>
+
+<script>
+export default{
+    name: "BlogTwo"
+}
+</script>
+
+
+<style lang="scss">
+@import "../../../assets/styles.scss"
+</style>

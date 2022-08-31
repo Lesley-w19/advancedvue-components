485daab28796c65491125b5ad31f10ed509cb1a3
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-08-31 23:56:40 +0300

    Added blog component

---
+++ src/components/pages/blogs/BlogOne.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h5>Javascript</h5>
+    <p>
+      As an aspiring web developer, it is important to follow trends in the
+      industry. Popular blogs are a way to keep track of emerging development
+      technologies, stay in touch with fellow developers, and learn modern web
+      development techniques. Tech blogs often have daily posts that can help
+      you learn basic knowledge of core developer skills like JavaScript. If you
+      are wondering where to learn JavaScript, know that there are many ways to
+      keep up with design systems and trends. This guide highlights the best
+      JavaScript blogs to follow if you want to keep up with new information in
+      the field. Keep reading to see the best JavaScript blogs to learn modern
+      techniques and improve your code writing skills.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogOne",
+};
+</script>
+
+<style lang="scss">
+@import "../../../assets/styles.scss";
+</style>

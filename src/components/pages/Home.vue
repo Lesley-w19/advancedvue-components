fae0af1809e0ef81e8819ba5aa31ad631a761eb9
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-01 08:44:26 +0300

    Changed the names of the blogs section

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,13 +1,20 @@
+<style lang="scss">
+@import "../../assets/styles.scss";
+</style>
+
 <template>
   <div class="px-5">
-    <h3 class="text-center">Home page</h3>
+    <!-- <h3 class="text-center">Home page</h3> -->
 
-    <div class="dynamic-components px-5">
+    <div class="hero-wrapper"></div>
+
+    <div class="dynamic-components px-5 my-5">
+      <h3 class="my-3 text-success">Articles</h3>
       <button @click="customComponent = 'BlogOne'" class="btn btn-primary mx-2">
-        Blog One
+        Javascript
       </button>
       <button @click="customComponent = 'BlogTwo'" class="btn btn-primary mx-2">
-        Blog Two
+        Development
       </button>
       <button
         @click="
@@ -16,13 +23,14 @@
         "
         class="btn btn-primary mx-2"
       >
-        Blog Three
+        CSS
       </button>
       <hr />
 
       <!-- Binding the current component to the property  -->
       <component :is="customComponent"></component>
     </div>
+   
   </div>
 </template>
 
@@ -35,6 +43,8 @@ export default {
   data() {
     return {
       customComponent: "BlogOne",
+      imglink:
+        "https://images.pexels.com/photos/3608056/pexels-photo-3608056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     };
   },
   components: {
@@ -46,6 +56,3 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss">
-@import "../../assets/styles.scss";
-</style>

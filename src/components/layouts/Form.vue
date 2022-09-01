8463ae8973bc86b52d92d4dc7e28db613e96d3a1
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-01 11:25:40 +0300

    added the form component

--- src/components/layouts/Form.vue
+++ src/components/layouts/Form.vue
@@ -1,22 +1,50 @@
 <template>
   <div class="form-wraper">
-    <h1>Fill the form:</h1>
+    <div>
+      <img :src="link" alt="form-pic" />
+    </div>
 
-    <form>
-      <div id="header">
-        <slot name="header"></slot>
-      </div>
+    <div>
+      <!-- <h1>Fill the form:</h1> -->
 
-      <div id="fields">
-        <slot name="fields"></slot>
-      </div>
-    </form>
+      <form>
+        <div id="header">
+          <slot name="header"></slot>
+        </div>
+
+        <div id="fields">
+          <slot name="fields"></slot>
+        </div>
+        <div id="button-div">
+          <slot name="button"></slot>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "  FormSetion",
+
+  data() {
+    return {
+      link: "https://images.pexels.com/photos/6373486/pexels-photo-6373486.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    };
+  },
+
+  mixins: [
+    //
+  ],
+  computed: {},
+  methods: { 
+    //you define your functions... trigger/used with events
+},
+  watchers: {
+    //case example : banks and their branches
+  },
+  components: {},
+  filters: {},
 };
 </script>
 

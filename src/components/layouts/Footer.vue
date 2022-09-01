8d588292d6c5e2d7314d21ae0b3837126c75721e
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-01 08:42:51 +0300

    added a new footer file

---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer-wrapper">
+    <div class="links">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact us</li>
+      </ul>
+    </div>
+    <div class="copy">
+      <p>&copy; <span>2022. All rights reserved.</span></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterSection",
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/styles.scss"
+</style>

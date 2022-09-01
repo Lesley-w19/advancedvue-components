6b3941e78d83b272fb465b2bb70d616b19505738
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-01 11:25:25 +0300

    Added the sign up form

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -1,13 +1,49 @@
 <template>
   <Form>
     <template #header>
-      <h1>Slot header</h1>
+      <h1 class="px-0">Sign Up</h1>
     </template>
     <template v-slot:fields>
-      <div>
-        <label for="">Name</label>
-        <input type="text" />
+      <div class="form-group mb-3">
+        <label for="exampleInputEmail1">Full name</label>
+        <input
+          type="text"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter ful name"
+        />
       </div>
+      <div class="form-group mb-3">
+        <label for="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+        />
+        <small id="emailHelp" class="form-text text-muted"
+          >We'll never share your email with anyone else.</small
+        >
+      </div>
+      <div class="form-group mb-3">
+        <label for="exampleInputPassword1">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+          placeholder="Password"
+        />
+      </div>
+
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+    </template>
+    <template #button>
+      <button type="submit" class="btn btn-primary my-2">Sign Up</button>
     </template>
   </Form>
 </template>
@@ -18,6 +54,11 @@ export default {
   name: "ContactSection",
   components: {
     Form,
+  },
+  data() {
+    return {
+      show: false,
+    };
   },
 };
 </script>

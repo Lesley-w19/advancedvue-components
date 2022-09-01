50afc6e5ecba2035416899174ce5f43387e3d4e9
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-01 08:43:40 +0300

    Added the form slot helper to be used in the contact page

---
+++ src/components/layouts/Form.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="form-wraper">
+    <h1>Fill the form:</h1>
+
+    <form>
+      <div id="header">
+        <slot name="header"></slot>
+      </div>
+
+      <div id="fields">
+        <slot name="fields"></slot>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "  FormSetion",
+};
+</script>
+
+<style></style>
---
+++ src/components/pages/Contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <Form>
+    <template #header>
+      <h1>Slot header</h1>
+    </template>
+    <template v-slot:fields>
+      <div>
+        <label for="">Name</label>
+        <input type="text" />
+      </div>
+    </template>
+  </Form>
+</template>
+
+<script>
+import Form from "../layouts/Form.vue";
+export default {
+  name: "ContactSection",
+  components: {
+    Form,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/styles.scss";
+</style>

8be8633856a3f0c1666a4dfacd6eda3e6108cadc
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-01 11:25:09 +0300

    Added the footer

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -30,7 +30,18 @@
       <!-- Binding the current component to the property  -->
       <component :is="customComponent"></component>
     </div>
-   
+    <div class="footer-wrapper">
+      <div class="links">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+      <div class="copy">
+        <p>&copy; <span>2022. All rights reserved.</span></p>
+      </div>
+    </div>
   </div>
 </template>
 

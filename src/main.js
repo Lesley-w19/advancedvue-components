70c522f341b933c42a76c7d354341128ab164ded
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-01 08:44:55 +0300

    Re-arranged the links to the different modules

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from "vue";
-import App from "./App.vue";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
